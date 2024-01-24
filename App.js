35ab98c50660171a26bac0e0dc4d72ba61cbea8a
Agustin Lopez <agustinmlopez1992@gmail.com>
2024-01-23 23:51:55 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>La app del Asado</Text>
+      <Text>Carrito</Text>
+      <Text>Chinchulines</Text>
+      <Text>Asado tira fina</Text>
+      <Text>Chorizo</Text>
+      <Text>Morcilla Salada</Text>
       <StatusBar style="auto" />
     </View>
   );
